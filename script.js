cec391ad55e2d106ca44ca3d7aabe64e06642f89
ori434 <oribornstein1@gmail.com>
2026-06-30 11:22:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ const grid = document.getelementbyid('grid');
 const scoredisplay = document.getelementbyid('score');
 let squares = {};
 let currentsnake = {2, 1, 0};
-let direction
-let
-let
-let
-let
+let direction = 1;
+let appleindex = 0;
+let score = 0;
+let timerid = 0;
+let intervaltime = 200;
+
+
+function createbored() {
+  for (let i = 0; i < 400; i++) {
+    connst square = document.createlement('div');
+    grid.appendchild(square);
+    squares.push(square);
+  }
+}
+creatbored();
