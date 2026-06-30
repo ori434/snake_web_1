17f2be03b5a8cbbb66c05a22f839fd611197ff04
ori434 <oribornstein1@gmail.com>
2026-06-30 10:56:06 +0300

    Initialize grid and score display in script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const grid = document.getelementbyid('grid');
+const scoredisplay = document.getelementbyid('score');
+let squares = {};
+let currentsnake = {2, 1, 0};
+let direction
+let
+let
+let
+let
