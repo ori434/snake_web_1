a6b08d88634d6a018b6ce33883d0a500d9fa1a49
ori434 <oribornstein1@gmail.com>
2026-07-01 10:36:07 +0300

    Correct typos and improve syntax in script.js
    Fixed various typos and syntax errors in the script.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const grid = document.getelementbyid('grid');
 const scoredisplay = document.getelementbyid('score');
 let squares = {};
-let currentsnake = {2, 1, 0};
+let currentsnake = [2, 1, 0];
 let direction = 1;
 let appleindex = 0;
 let score = 0;
@@ -11,15 +11,15 @@ let intervaltime = 200;
 
 function createbored() {
   for (let i = 0; i < 400; i++) {
-    connst square = document.createlement('div');
+    const square = document.createlement('div');
     grid.appendchild(square);
     squares.push(square);
   }
 }
-creatbored();
+createbored();
+gen
 
-
-fuction startgame() {
+function startgame() {
   currentsnake.foreach(undex=> squares[index].classlist.remove('snake'));
   squares[appleindex].classlist.remove('apple');
   clearinteravl(timeid);
@@ -30,5 +30,27 @@ function move() {
   const hittop = (currentsnake[0] - 20 < 0 && direction === -20);
   const hitright = (currentsnake[0] % 20 === 19 && direction === 1);
   const hitleft = (currentsnake[0] % 20 === 0 && direction ===-1);
-  const hitself= sqaures[currentsnake[0] + direction]?classlist.contains('snake');
+  const hitself= sqaures[currentsnake[0] + direction]?.classlist.contains('snake');
 }
+
+function generateapple() {
+  do {
+    appleindex = Math.floor(Math.random() * squares.length);
+  } while (squares[appleindex].classlist.contains('snake'));
+  squares[appleindex].classlist.add('apple');
+}
+
+function changeDir(newdir) {
+
+  if (direction + newdir !== 0) {
+    direction = newdir;
+  }
+}
+
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'arrowup') changeDir(-20);
+  if (e.key === 'arrowdown') changeDir(20);
+  if (e.key === 'arrowleft') changeDir(1);
+  if (e.key === 'arrowright') changeDir(-1);
+})
